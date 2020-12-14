2facaea1c7e7dbe76d7dc56c3e394ef53b624846
Your Name <darwinmuthu450@gmail.com>
2020-12-14 14:48:02 +0530

    landing page finshed

---
+++ src/components/pages/aboutUsPage/AboutUsPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GetInTouch from "./components/getInTouch/GetInTouch";
+import ProfileCard from "./components/profileCard/ProfileCard";
+
+export default function AboutUsPage() {
+  return (
+    <>
+      <ProfileCard />
+      <GetInTouch />
+    </>
+  );
+}
--- src/components/pages/landingPage/LandingPage.js
+++ src/components/pages/landingPage/LandingPage.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./landingPage.scss";
 import Header from "../../commonComponents/header/Header";
+import girlOnBooks from "../../../images/girl-on-books.svg";
 export default function LandingPage() {
   return (
     <>
       <Header />
+      <div className="landingPage">
+        <div className="landingPage-left">
+          <h1 className="landingPage-left-head">
+            Free <span className="landingPage-left-head-EBooks">EBooks</span>
+          </h1>
+          <p className="landingPage-left-para">
+            Download Free EBooks in PDF format. No annoying ads, no download
+            limits, enjoy it and don't forget to bookmark and share the love!
+          </p>
+          <p className="landingPage-left-button">Get Books</p>
+        </div>
+        <div className="landingPage-right">
+          <img className="landingPage-right-img" src={girlOnBooks} />
+        </div>
+      </div>
     </>
   );
 }
